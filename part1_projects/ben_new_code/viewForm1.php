4d16880dafe5368e4fe580052ddd721b02e89679
bengiangrasso <32199973+bengiangrasso@users.noreply.github.com>
2019-04-23 17:18:05 -0400

    Update viewForm1.php

--- part1_projects/ben_new_code/viewForm1.php
+++ part1_projects/ben_new_code/viewForm1.php
@@ -3,52 +3,6 @@
   <title>GWU Advising</title>
 <link rel="icon" href="http://www.gwrha.com/uploads/1/7/9/9/17997469/gw_atx_4cp_pos.png">
   <link rel="stylesheet" href="style.css">
-<style>
-.button {
-  display: inline-block;
-  border-radius: 4px;
-  background-color: #000000;
-  border: none;
-  color: #FFFFFF;
-  text-align: center;
-  font-size: 20px;
-  padding: 20px;
-  width: 200px;
-  transition: all 0.5s;
-  cursor: pointer;
-  margin: 5px;
-}
-.button span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-}
-.button span:after {
-  content: '\00bb';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -20px;
-  transition: 0.5s;
-}
-.button:hover span {
-  padding-right: 25px;
-}
-.button:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-table {
-  border-collapse: collapse;
-  width: 100%;
-}
-th, td {
-  padding: 8px;
-  text-align: left;
-  border-bottom: 1px solid #ddd;
-}
-</style>
 </head>
 <body>
   <h1>View Form 1</h1>
@@ -62,14 +16,14 @@ th, td {
 	}
 	
 	session_start();
-	        $id = $_SESSION["id"];
+	        $uid = $_SESSION["uid"];
   		$role = $_SESSION["role"];
-	$student_id = $_POST["id"];
+	$student_id = $_POST["uid"];
 	
 	    $servername = "localhost";
-	    $username = "TeamEighteen";
-	    $password = "DatabasePassword1!";
-	    $dbname = "TeamEighteen";
+	    $username = "Team_Name";
+	    $password = "p@ssW0RD";
+	    $dbname = "Team_Name";
 
       // define connection variable
       $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -78,7 +32,7 @@ th, td {
         die("Connection failed: " . mysqli_connect_error());
       }
       // define the sql_insert_query
-      $query = "SELECT * FROM formOne WHERE id = ".$student_id."";
+      $query = "SELECT * FROM formOne WHERE uid = ".$student_id."";
       $result = mysqli_query($conn, $query) or die("Bad Query: $query");
       if(mysqli_num_rows($result) > 0){
       	echo "<table border='1'>";
@@ -111,7 +65,7 @@ th, td {
 	}
 	
 	echo '<form action="'.$destination.'.php" method = "post">';
-        echo '<input type="hidden" name="id" value = "'.$id.'"/>';
+        echo '<input type="hidden" name="id" value = "'.$uid.'"/>';
 	echo '<input type="hidden" name="permission" value = 1/>';
         echo '<button class="button" style="vertical-align:middle"><span>Return to Home Page</span></button>';
   	echo '</form>';
