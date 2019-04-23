bb611da2688393b91683c2949d5535652b89ca6f
bengiangrasso <32199973+bengiangrasso@users.noreply.github.com>
2019-04-23 17:42:49 -0400

    Update editPersonalInfo.php

--- part1_projects/ben_new_code/editPersonalInfo.php
+++ part1_projects/ben_new_code/editPersonalInfo.php
@@ -4,56 +4,11 @@
     <title>GWU Advising</title>
   <link rel="icon" href="http://www.gwrha.com/uploads/1/7/9/9/17997469/gw_atx_4cp_pos.png">
 <link rel="stylesheet" href="style.css">
-<style>
-.button {
-  display: inline-block;
-  border-radius: 4px;
-  background-color: #000000;
-  border: none;
-  color: #FFFFFF;
-  text-align: center;
-  font-size: 20px;
-  padding: 20px;
-  width: 200px;
-  transition: all 0.5s;
-  cursor: pointer;
-  margin: 5px;
-}
-.button span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-}
-.button span:after {
-  content: '\00bb';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -20px;
-  transition: 0.5s;
-}
-.button:hover span {
-  padding-right: 25px;
-}
-.button:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-table {
-  border-collapse: collapse;
-  width: 100%;
-}
-th, td {
-  padding: 8px;
-  text-align: left;
-  border-bottom: 1px solid #ddd;
-}
-</style>
+
 </head>
 <?php
   session_start();
-  $id = $_SESSION["id"];
+  $uid = $_SESSION["uid"];
   $role = $_SESSION["role"];
   $fname = $_POST["fname"]; 
   $lname = $_POST["lname"]; 
@@ -72,19 +27,19 @@ th, td {
 		   die("Connection failed:".mysqli_connect_error());	
 	}		
 	if (isset($fname)){
-	  $query = "UPDATE users SET fname='$fname' WHERE id='$id'";
+	  $query = "UPDATE user SET fname='$fname' WHERE uid='$uid'";
 	  $result=mysqli_query($conn,$query); 
 	}
 	if (isset($lname)){
-	  $query2 = "UPDATE users SET lname='$lname' WHERE id='$id'";
+	  $query2 = "UPDATE user SET lname='$lname' WHERE uid='$uid'";
 	  $result2=mysqli_query($conn,$query2); 
 	}
 	if (isset($email)){
-	  $query3 = "UPDATE users SET email='$email' WHERE id='$id'";
+	  $query3 = "UPDATE user SET email='$email' WHERE uid='$uid'";
 	  $result3=mysqli_query($conn,$query3); 
 	}
 	if (isset($address)){
-	  $query4 = "UPDATE users SET address='$address' WHERE id='$id'";
+	  $query4 = "UPDATE user SET city='$address' WHERE uid='$uid'";
 	  $result4=mysqli_query($conn,$query4); 
 	}
         mysqli_close($conn);
