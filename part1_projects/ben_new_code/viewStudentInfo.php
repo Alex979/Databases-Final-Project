f8a9ec6b729972508999e5ee0e10bd520f1cc80f
bengiangrasso <32199973+bengiangrasso@users.noreply.github.com>
2019-04-23 17:14:50 -0400

    Update viewStudentInfo.php

--- part1_projects/ben_new_code/viewStudentInfo.php
+++ part1_projects/ben_new_code/viewStudentInfo.php
@@ -3,52 +3,6 @@
   <title>GWU Advising</title>
   <link rel="icon" href="http://www.gwrha.com/uploads/1/7/9/9/17997469/gw_atx_4cp_pos.png">
   <link rel="stylesheet" href="style.css">
-<style>
-.button {
-  display: inline-block;
-  border-radius: 4px;
-  background-color: #000000;
-  border: none;
-  color: #FFFFFF;
-  text-align: center;
-  font-size: 20px;
-  padding: 20px;
-  width: 200px;
-  transition: all 0.5s;
-  cursor: pointer;
-  margin: 5px;
-}
-.button span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-}
-.button span:after {
-  content: '\00bb';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -20px;
-  transition: 0.5s;
-}
-.button:hover span {
-  padding-right: 25px;
-}
-.button:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-table {
-  border-collapse: collapse;
-  width: 100%;
-}
-th, td {
-  padding: 8px;
-  text-align: left;
-  border-bottom: 1px solid #ddd;
-}
-</style>
 </head>
 <h1>Student Information</h1>
 
@@ -56,7 +10,7 @@ th, td {
 
 <?php
   session_start();
-  $id = $_SESSION["id"];
+  $id = $_SESSION["uid"];
   $role = $_SESSION["role"];
 
 	$permission = 0;
@@ -68,9 +22,9 @@ th, td {
 
 	/* Create connection */
 	$servername = "localhost";
-	$username = "TeamEighteen";
-	$password = "DatabasePassword1!";
-	$dbname = "TeamEighteen";	
+	$username = "Team_Name";
+	$password = "p@ssW0RD";
+	$dbname = "Team_Name";	
 	
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 		  
@@ -79,9 +33,9 @@ th, td {
 		die("Connection failed: ".mysqli_connect_error());
 	}
 	  
-	$id = $_POST["id"];
+	$uid = $_POST["uid"];
 
-	$student_query = "SELECT * FROM users WHERE id LIKE '%".$id."%'";
+	$student_query = "SELECT * FROM user WHERE uid LIKE '%".$uid."%'";
 	$student_result = mysqli_query($conn,$student_query);
 	if (mysqli_num_rows($student_result) > 0)
 	{
@@ -90,7 +44,7 @@ th, td {
 			$firstname = $student_row["fname"];
 			$lastname = $student_row["lname"];
       			$email = $student_row["email"];
-      			$address = $student_row["address"];
+      			$address = $student_row["city"];
       			$balance = $student_row["balance"];
 		}
 	}
@@ -98,7 +52,7 @@ th, td {
   echo 'Name: '.$firstname.' '.$lastname.'<br>';
   echo 'University ID: '.$id.'<br>';
   echo 'Email: '.$email.'<br>';
-  echo 'Address: '.$address.'<br>';
+  echo 'Address: '.$city.'<br>';
   echo 'Balance: $'.$balance.'<br>';
   echo '<form action="student.php" method = "post"></form>';
   echo '<br><br><br>';
