9d1e3e32f42405b36e598d853c7dcdbe18617edb
bengiangrasso <32199973+bengiangrasso@users.noreply.github.com>
2019-04-24 00:27:36 -0400

    Create successSubmit.php

---
+++ part1_projects/ben_new_code/successSubmit.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Success</title>
+  <link rel="icon" href="http://www.gwrha.com/uploads/1/7/9/9/17997469/gw_atx_4cp_pos.png">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1>GWU Advising</h1>
+  <p>
+    Your submission was a success!
+    <a href="student.php">Go Back</a><br>
+  </p>
+</body>
+</html>
