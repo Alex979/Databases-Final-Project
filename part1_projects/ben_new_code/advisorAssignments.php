82f9295ef3ab0320204f0fe9a3765f406d6f3655
bengiangrasso <32199973+bengiangrasso@users.noreply.github.com>
2019-04-23 21:25:04 -0400

    Update advisorAssignments.php

--- part1_projects/ben_new_code/advisorAssignments.php
+++ part1_projects/ben_new_code/advisorAssignments.php
@@ -3,17 +3,7 @@
  <title>GWU Advising</title>
   <link rel="stylesheet" href="style.css">
 	<link rel="icon" href="http://www.gwrha.com/uploads/1/7/9/9/17997469/gw_atx_4cp_pos.png">
-	<style>
-table {
-  border-collapse: collapse;
-  width: 100%;
-}
-th, td {
-  padding: 8px;
-  text-align: left;
-  border-bottom: 1px solid #ddd;
-}
-	</style>
+
 </head>
 <body>
 <h1>Assign an advisor to a student</h1>
@@ -81,10 +71,10 @@ th, td {
 			{	
 				$student_fname = $row["fname"];
 				$student_lname = $row["lname"];
-				$student_id = $row["id"];
+				$student_id = $row["uid"];
 				$advisor_id = $row["advisorid"];
 			 
-				$innerquery = "SELECT * FROM users WHERE id = ".$advisor_id."";
+				$innerquery = "SELECT * FROM user WHERE uid = ".$advisor_id."";
 				$innerresult = mysqli_query($conn,$innerquery);
 				if (mysqli_num_rows($result) > 0)
 				{
