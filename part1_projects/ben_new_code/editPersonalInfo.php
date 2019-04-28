94895ab290de3226e8066bbc7a67b0625cbf72ad
Alex Broaddus <alexbroaddus1@gmail.com>
2019-04-28 14:32:49 -0400

    Made editpersonalinfo page up to date with new UI style

--- part1_projects/ben_new_code/editPersonalInfo.php
+++ part1_projects/ben_new_code/editPersonalInfo.php
@@ -6,7 +6,13 @@
 
     <title>GWU Advising</title>
   <link rel="icon" href="http://www.gwrha.com/uploads/1/7/9/9/17997469/gw_atx_4cp_pos.png">
-<link rel="stylesheet" href="style.css">
+
+	<!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 <?php
@@ -66,42 +72,86 @@
         mysqli_close($conn);
   ?>
 <body>
-<?php
-echo '<h2>Edit your personal information</h2>';
-echo '<form action="editPersonalInfo.php" method="post"><b>Update: </b><br><br>';
-    echo '<b>First Name: </b> <br><input type="text" ID="fname" name="fname"><br>';
-    echo '<b>Last Name: </b> <br><input type="text" ID="lname" name="lname"><br>';
-    echo '<b>Email: </b> <br><input type="text" ID="email" name="email"><br>';
-    echo '<b>Street: </b> <br><input type="text" ID="street" name="street" ><br>';
-    echo '<b>City: </b> <br><input type="text" ID="city" name="city" ><br>';
-    echo '<b>State: </b> <br><input type="text" ID="state" name="state" ><br>';
-    echo '<b>Zipcode: </b> <br><input type="text" ID="zip" name="zip" ><br><br>';
-    echo '<button class="button" style="vertical-align:middle"><span>Update</span></button>';
-  echo '</form>';
+	<?php
+  include('../FlatEarthSociety/public_html/navbar.php');
+  ?>
+	<div class="container mt-3">
+	<?php
+	echo '<h1 class="text-primary">Edit your personal information</h1>';
+	echo '<form action="editPersonalInfo.php" method="post" style="max-width: 500px" >';
+			echo '
+			<div class="form-group">
+				<label>First Name</label>
+				<input class="form-control" type="text" ID="fname" name="fname">
+			</div>
+			';
+			echo '
+			<div class="form-group">
+				<label>Last Name</label>
+				<input class="form-control" type="text" ID="lname" name="lname">
+			</div>
+			';
+			echo '
+			<div class="form-group">
+				<label>Email</label>
+				<input class="form-control" type="text" ID="email" name="email">
+			</div>
+			';
+			echo '
+			<div class="form-group">
+				<label>Street</label>
+				<input class="form-control" type="text" ID="street" name="street" >
+			</div>
+			';
+			echo '
+			<div class="form-group">
+				<label>City</label>
+				<input class="form-control" type="text" ID="city" name="city" >
+			</div>
+			';
+			echo '
+			<div class="form-group">
+				<label>State</label>
+				<input class="form-control" type="text" ID="state" name="state" >
+			</div>
+			';
+			echo '
+			<div class="form-group">
+				<label>Zip Code</label>
+				<input class="form-control" type="text" ID="zip" name="zip" >
+			</div>
+			';
+			echo '<button class="btn btn-primary" style="vertical-align:middle"><span>Update</span></button>';
+		echo '</form>';
 
-	switch ($role) {
-    	case "student":
-		$destination = student;
-       		 break;
-    	case "advisor":
-		$destination = advisor;
-        	break;
-   	case "gradSec":
-        	$destination =  gradsec;
-        	break;
-	case "alumni":
-        	$destination =  alumni;
-        	break;
-	case "sysAdmin":
-        	$destination = sysadmin;
-        	break;
-	}
-	
-	echo '<form action="../FlatEarthSociety/public_html/dashboard.php" method = "post">';
-        echo '<input type="hidden" name="uid" value = "'.$uid.'"/>';
-	echo '<input type="hidden" name="permission" value = 1/>';
-        echo '<button class="button" style="vertical-align:middle"><span>Return to Home Page</span></button>';
-  	echo '</form>';
-     ?>
+		switch ($role) {
+				case "student":
+			$destination = student;
+						break;
+				case "advisor":
+			$destination = advisor;
+						break;
+			case "gradSec":
+						$destination =  gradsec;
+						break;
+		case "alumni":
+						$destination =  alumni;
+						break;
+		case "sysAdmin":
+						$destination = sysadmin;
+						break;
+		}
+		
+		echo '<form action="../FlatEarthSociety/public_html/dashboard.php" method = "post">';
+					echo '<input type="hidden" name="uid" value = "'.$uid.'"/>';
+		echo '<input type="hidden" name="permission" value = 1/>';
+					echo '<button class="btn btn-primary" style="vertical-align:middle"><span>Return to Home Page</span></button>';
+			echo '</form>';
+			?>
+	</div>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
