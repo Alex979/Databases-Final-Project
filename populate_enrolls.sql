ed1606149f7d8a171705a5ba62f25b6fa0497655
Alex Broaddus <alexbroaddus1@gmail.com>
2019-05-03 13:51:05 -0400

    Populated courses for past semester student

--- populate_enrolls.sql
+++ populate_enrolls.sql
@@ -6,3 +6,44 @@ INSERT INTO enrolls VALUES (
 INSERT INTO enrolls VALUES (
     88888888, 3, NULL
 );
+
+-- Populate courses of student ready to graduate
+INSERT INTO enrolls VALUES (
+    10101010, 23, NULL
+);
+
+INSERT INTO enrolls VALUES (
+    10101010, 21, NULL
+);
+
+INSERT INTO enrolls VALUES (
+    10101010, 22, NULL
+);
+
+INSERT INTO enrolls VALUES (
+    10101010, 24, NULL
+);
+
+INSERT INTO enrolls VALUES (
+    10101010, 25, NULL
+);
+
+INSERT INTO enrolls VALUES (
+    10101010, 26, NULL
+);
+
+INSERT INTO enrolls VALUES (
+    10101010, 27, NULL
+);
+
+INSERT INTO enrolls VALUES (
+    10101010, 28, NULL
+);
+
+INSERT INTO enrolls VALUES (
+    10101010, 29, NULL
+);
+
+INSERT INTO enrolls VALUES (
+    10101010, 30, NULL
+);
