e2024adb7986e3cbea635784a7575d07e513860a
Alex Broaddus <alexbroaddus1@gmail.com>
2019-05-03 13:55:39 -0400

    Added grades

--- populate_enrolls.sql
+++ populate_enrolls.sql
@@ -9,41 +9,41 @@ INSERT INTO enrolls VALUES (
 
 -- Populate courses of student ready to graduate
 INSERT INTO enrolls VALUES (
-    10101010, 23, NULL
+    10101010, 23, 'A'
 );
 
 INSERT INTO enrolls VALUES (
-    10101010, 21, NULL
+    10101010, 21, 'A-'
 );
 
 INSERT INTO enrolls VALUES (
-    10101010, 22, NULL
+    10101010, 22, 'A'
 );
 
 INSERT INTO enrolls VALUES (
-    10101010, 24, NULL
+    10101010, 24, 'A'
 );
 
 INSERT INTO enrolls VALUES (
-    10101010, 25, NULL
+    10101010, 25, 'A'
 );
 
 INSERT INTO enrolls VALUES (
-    10101010, 26, NULL
+    10101010, 26, 'A'
 );
 
 INSERT INTO enrolls VALUES (
-    10101010, 27, NULL
+    10101010, 27, 'A'
 );
 
 INSERT INTO enrolls VALUES (
-    10101010, 28, NULL
+    10101010, 28, 'A'
 );
 
 INSERT INTO enrolls VALUES (
-    10101010, 29, NULL
+    10101010, 29, 'A'
 );
 
 INSERT INTO enrolls VALUES (
-    10101010, 30, NULL
+    10101010, 30, 'A'
 );
