0ace9c1fb474b31bddababa76ea8c0db19848f5e
bengiangrasso <32199973+bengiangrasso@users.noreply.github.com>
2019-05-03 14:04:14 -0400

    Update dashboard.php

--- part1_projects/FlatEarthSociety/public_html/dashboard.php
+++ part1_projects/FlatEarthSociety/public_html/dashboard.php
@@ -60,7 +60,7 @@ function create_link($title, $description, $link_text, $url) {
                     if (in_array("student", $_SESSION["role"])) {
                         create_link("Courses", "View all the courses offerred.", "Go to course list", "courses.php");
                         create_link("Transcript", "View the courses you currently are taken and have taken in the past.", "View transcript", "transcript.php");
-                        create_link("Apply To Graduate", "Apply to graduate.", "Go to application", "../../ben_new_code/applyToGraduate.php");
+                        create_link("Apply To Graduate", "Apply to graduate.", "Go to application", "../../ben_new_code/gradForm.php");
                         create_link("Submit Form 1", "Submit the form required to graduate.", "Go to form 1", "../../ben_new_code/form1Submit.php");
                         create_link("View Form 1", "View your form 1 submission.", "View form 1", "../../ben_new_code/viewForm1.php");
                     }
