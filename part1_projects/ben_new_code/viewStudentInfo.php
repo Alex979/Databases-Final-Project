7ffa43b70619d0e3a8239afce608134d60f13e8e
bengiangrasso <32199973+bengiangrasso@users.noreply.github.com>
2019-05-04 15:10:26 -0400

    Update viewStudentInfo.php

--- part1_projects/ben_new_code/viewStudentInfo.php
+++ part1_projects/ben_new_code/viewStudentInfo.php
@@ -1,15 +1,24 @@
+<?php
+  session_start();
+?>
 <html>
 <head>
   <title>GWU Advising</title>
   <link rel="icon" href="http://www.gwrha.com/uploads/1/7/9/9/17997469/gw_atx_4cp_pos.png">
-  <link rel="stylesheet" href="style.css">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+	<!-- Custom styles for this template -->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 <h1>Student Information</h1>
 
 <body>
-
 <?php
-  session_start();
+  include('../FlatEarthSociety/public_html/navbar.php');
+  ?>
+	<div class="container mt-3">
+	<h1 class="text-primary">Student Information</h1>
+<?php
   $id = $_SESSION["uid"];
   $role = $_SESSION["role"];
 
@@ -83,5 +92,9 @@ switch ($role) {
 
 mysqli_close($conn);
  ?>
+		</div>
+ 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</body>
 	</html>
