4dc44ec366b770b8ff67a83216af798c8dd516fc
bengiangrasso <32199973+bengiangrasso@users.noreply.github.com>
2019-05-04 16:00:48 -0400

    Create gradSuccess.php

---
+++ part1_projects/ben_new_code/gradSuccess.php
@@ -0,0 +1,29 @@
+<?php
+
+	$servername = "localhost";
+	$username = "Team_Name";
+	$password = "p@ssW0RD";
+	$dbname = "Team_Name";	
+	
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+		  
+	if (!$conn) //Check if connection failed
+	{	
+		die("Connection failed: ".mysqli_connect_error());
+	}
+	
+	$newGrad = 0;
+	$newGrad = $_POST["newGrad"];
+	if ($newGrad == 1)
+	{
+		$studentid = $_POST["id"];
+		
+		$query = "UPDATE roles SET role = 'alumni' WHERE id = '$studentid'";
+		 $result = mysqli_query($conn,$query);
+     
+     		$query2 = "DELETE FROM formOne WHERE id = '$studentid'";
+		 $result2 = mysqli_query($conn,$query2);
+    
+    header("Location: ../FlatEarthSociety/public_html/dashboard.php");
+	}
+?>
